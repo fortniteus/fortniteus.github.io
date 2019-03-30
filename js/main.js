95a4fd90b970fa5d53142f6a3b7a8f1c62cb74b4
fortniteUSA <twsociablog@gmail.com>
2019-03-30 14:40:39 +0100

    apex

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 /*!
+*  Roblox Niche
  * Main.js - Hearth of Application
- * Copyright 2013-2017 CPACodex.com, Inc.
+ * Copyright 2013-2018 CPACodex.com, Inc.
  * Licensed under the MIT license
  */
-$( document ).ready( function() {  
 
+$( document ).ready( function() {
+	$("[name='my-checkbox']").bootstrapSwitch();
+
+    $('[data-toggle="tooltip"]').tooltip();
 
     setInterval( newActivity, 2000 );
+
 
     function newActivity() {
 
@@ -15,155 +20,162 @@ $( document ).ready( function() {
         var users   = [ '441Binder','Johnny', 'Attacklord_bro', 'pewdiepie', "Marko", "PesaJajar", "Ivan", "xxMine", "9814Bind", "P!xel", "haxhack", "EarthHacker", "9/4gen", "ClashHacker", 'new_kid',
                   'BloodMaster', 'lovegaming', 'Johnny', 'NaneK', 'Marko_991', 'JackHammer9999', 'fuzzzy', 'tester', 'yearboy1007', 'Ministar', 'Haxajmo', 'Pottato', 'GirlHackingxD', 'Alex',
                   'Coder', 'Hillck23', 'Creeper', 'zaCk', 'only123', 'gunshaxer', 'MyNameIsShoost' ];
-        var avatars = [ 'https://i.imgur.com/V4GhVTn.jpg',
-                        'https://i.imgur.com/ZJf7aoN.png',
-                        'https://i.imgur.com/htRO3QO.jpg',
-                        'https://i.imgur.com/6C7X3TM.jpg',
-                        'https://i.imgur.com/n8v5Mr7.png',
-                        'https://i.imgur.com/1MV2ViL.png',
-                        'https://i.imgur.com/VPPMg0b.png',
-                        'https://i.imgur.com/6ECGSEe.jpg',
-                        'https://i.imgur.com/e7lWcDe.jpg'];
+        var platforms = [ '<i class="fab fa-windows orange_color"></i>', '<i class="fab fa-playstation orange_color"></i>', '<i class="fab fa-steam orange_color"></i>' ];
 
-        var flag         = flags[ Math.floor( Math.random() * flags.length ) ];
-        var avatar       = avatars[ Math.floor( Math.random() * avatars.length ) ];
-        var user         = users[ Math.floor( Math.random() * users.length ) ];
-        var vBucksRand     = Math.floor( ( Math.random() * 500 ) + 50 );;
+        var battlepointsRand 	 = ( parseInt( Math.floor( ( Math.random() * 100000 ) + 10000 ) / 1000, 10 ) +1 ) * 1000;
+        var flag                 = flags[ Math.floor( Math.random() * flags.length ) ];
+        var platform             = platforms[ Math.floor( Math.random() * platforms.length ) ];
+        var user                 = users[ Math.floor( Math.random() * users.length ) ];
+        var tixRand              = Math.floor( ( Math.random() * 5000 ) + 500 );
 
-        $( '.live-stats' ).last().remove();
 
-        $( '.activityContent' ).hide().prepend('<div class="live-stats">' +
-            '<div class="avatar"><img src="'+avatar+'" alt="" /></div>' +
-            '<div class="flag"><img src="img/flags/'+flag+'.png" alt="" /></div>' +
-           '<div class="info">' +
-              '<ul>' +
-                 '<li><b>'+user+',</b> generated now:</li>' +
-                 '<li><img class="item-gen" src="img/vBucks.png" alt="" />V-bucks: <span class="bucks-color">' + vBucksRand + '</span> </li>' +
-              '</ul>' +
-           '</div>' +
+        $( '.activity' ).last().remove();
+
+        $( '.activityContent' ).hide().prepend('<div class="activity">' +
+           '<ul>' +
+           		'<li><img class="avatar" src="img/avatar.jpg" alt="avatar" /><img class="flag" src="img/flags/'+flag+'.png" alt="flag" /></li>' +
+           		'<li><span>Name</span><br />'+user+'</li>' +
+                '<li><span>Battle Points</span><br /><img class="item-icon" src="img/bp.png"> <b>'+battlepointsRand+'</b></li>' +
+                '<li><span>Platform</span><br />'+platform+'</li>' +
+           '</ul>' +
         '</div>').fadeIn();
     }
 
-    /* vBucks */
-    var $gen_vBucks = $( '.vBucks-input' );
-        $gen_vBucks.updown ({
+	$( '.platforms li' ).click( function() {
 
-        	step: 50,
-            min: 150,
-            max: 1000,
+		$( '.platforms li' ).removeClass( "active_platform" );
+	    $( this ).addClass("active_platform");
+
+	});		
+
+
+	/* Battle Points - change step, and max if you want other values */
+    var $gen_bpoints = $( '.bpoints-input' );
+        $gen_bpoints.updown ({
+
+        	step: 1000,
+            min: 10000,
+            max: 90000
 
         });
 
-    var $updown_vBucks = $gen_vBucks.data( 'updown');
-        $( '.plus-vBucks' ).click( function( event ) {
+    var $bpoints = $gen_bpoints.data( 'updown');
+        $( '.plus-bpoints' ).click( function( event ) {
 
-            $updown_vBucks.increase( event );
-            $updown_vBucks.triggerEvents();
+            $bpoints.increase( event );
+            $bpoints.triggerEvents();
 
         });
-        $( '.minus-vBucks' ).click( function( event ) {
+        $( '.minus-bpoints' ).click( function( event ) {
 
-            $updown_vBucks.decrease( event );
-            $updown_vBucks.triggerEvents();
+            $bpoints.decrease( event );
+            $bpoints.triggerEvents();
 
-        }); 
+        });
+        
+
+    function goToByScroll( id ) {
+
+        $( 'html, body' ).animate({
+            scrollTop: $( "#"+id ).offset().top },
+           'slow' );
+
+    }
+
+    $( '.btnVerify' ).click( function() {
+
+        $( '.generator_page_3' ).fadeOut( function() {
+
+            $( '.generatorTxt' ).html( 'Battle Points <span class="orange_color"><img class="item-icon" src="img/bp.png"> </span><span class="orange_color bpJS">10</span>' );
+
+            $( '.generator_page_2' ).fadeIn( 1000 );
+
+                $('.bpJS').countTo({
+
+                    from: 10,
+                    to: $( '.bpoints-input' ).val(),
+                    speed: 3000,
+                    refreshInterval: 1,
+                    onComplete: function( value ) {     
+
+                        setTimeout( function() {
+
+                            $( '.generator_page_2' ).fadeOut( function() {
+
+                                $( '.generator_page_4' ).fadeIn( 1000 );
+
+                            });
+
+                        }, 2000 );
+
+                    }
+
+                });       
 
 
-    function progressBar() {
 
-        var width            = 1;
-        var intervalProgress = setInterval( frame, 0200 );
-
-        goToByScroll( 'generatorJS' );
-
-        function frame() {
-
-            if( width == 20 ) $( '.load-textJS' ).html( 'Connecting' );
-            if( width == 30 ) $( '.load-textJS' ).html( 'Fetching data' );
-            if( width == 40 ) $( '.load-textJS' ).html( 'GET = Variables' );
-            if( width == 50 ) $( '.load-textJS' ).html( 'GET -> Username' );
-            if( width == 60 ) $( '.load-textJS' ).html( 'GET -> IP Address' );
-            if( width == 70 ) $( '.load-textJS' ).html( 'GET -> vBucks' );
-            if( width == 80 ) $( '.load-textJS' ).html( 'POST => DATA' );
-
-            if ( width >= 100 ) {
-
-                clearInterval( intervalProgress );
-                $( '.generator-step2' ).slideUp();
-                $( '.generator-step3' ).slideDown();
-
-                $( '.userJS' ).html( $( '.username-input' ).val() );
-
-                            $('.vBucksJS').countTo({
-
-                                from: 10,
-                                to: $( '.vBucks-input' ).val(),
-                                speed: 1000,
-                                refreshInterval: 1,
-                                onComplete: function ( value) {
-
-                                    $( '.vBucksJSthick' ).css('opacity', '1');
-
-                                        setTimeout( function() { 
-
-                                            $( '.generator-step3' ).fadeOut( function() {
-
-                                                $( '.generator-offers' ).fadeIn();
-
-                                            });
-                                            
-                                        }, 2000);                                
-
-                                }
-
-                              
-
-                        
-                  });               
-
-            } else {
-
-                width++; 
-                $( ".progress-bar" ).css( "width", width + '%' );
-                $( ".progress-bar" ).html( width * 1 + '%' );
-
-            }
-
-        }
-
-    }   
-    
-    /* Generator Starts Here */
-
-    $( ".btnGenerator" ).click( function() {
-
-    	var $player_name = $( '.username-input' ).val();
-
-    	if( $player_name == "" || $player_name.replace(/ /g,'').length < 2 ) {
-
-    		$( ".username-input" ).css( { border: '1px solid #F00' } );
-
-    	} else {
-
-    		$( ".username-input" ).css( { border: '1px solid #111319' } );	// Change border to default
-
-    		$( '.generator' ).slideUp( function() {
-
-    			$( '.generator-step2' ).slideDown();
-                progressBar();
-
-    		});
-
-    	}
+        });        
 
     });
-    // .btngenerator end
 
-    /* Button to scroll to Generator */
+    $( '.btnGenerate' ).click( function() {
 
-    $( '.btn-GoGenerator' ).click( function() {
+        var usernameInput  = $( '.usernameInput' ).val();
 
-        goToByScroll( 'generator' );
+        if( usernameInput == "" ) {
+
+            $( '.usernameAlert' ).fadeIn();
+
+        } else {
+
+            $( '.usernameAlert' ).fadeOut();
+
+            $( '.generator_page_1' ).fadeOut( function () {
+
+                $( '.generator_page_2' ).fadeIn( 1000 );
+
+                setTimeout( function() {
+
+                    $( '.generatorTxt' ).html( 'Connecting to server<span class="blink">...</span>' );
+
+                    setTimeout( function() {
+
+                        $( '.generatorTxt' ).html( 'Exporting database<span class="blink">...</span>' );
+
+                         setTimeout( function() {
+
+                            $( '.generatorTxt' ).html( 'Importing data info<span class="blink">...</span>' );
+
+                            setTimeout( function() {
+
+                                $( '.generatorTxt' ).html( 'Connecting Player: <span class="orange_color">'+usernameInput+'</span>' );
+
+                                $( '.generatorUsername' ).html( usernameInput );
+
+                                setTimeout( function() {
+
+                                    $( '.generator_page_2' ).fadeOut( function() {
+
+                                        $( '.generator_page_3' ).fadeIn( 1000 );
+
+                                    });
+
+                                }, 1500 );                                   
+
+                            }, 3000 );                              
+
+                        }, 3000 );                         
+
+                    }, 3000 );                
+
+                }, 3000 );
+
+            });
+
+        }   
 
     });
+																		
+				
+
 });
